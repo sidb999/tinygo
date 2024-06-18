261c639fc38c672f429602b275daaa17d7b66ed4
sidd <user@o0.tel>
2024-06-18 14:48:18 +0300

    test commit

--- flash.sh
+++ flash.sh
@@ -2,3 +2,4 @@
 # tinygo flash -monitor -baudrate=9600 -target arduino-nano -scheduler tasks
 # tinygo flash -monitor -baudrate=9600 -serial=usb -target=xiao -scheduler=tasks
 # tinygo flash -monitor -baudrate=9600 -target=xiao -scheduler=tasks
+# tinygo flash -monitor -baudrate=9600 -target=xiao -scheduler=tasks
